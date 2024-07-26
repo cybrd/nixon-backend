7f5d967c932293963e50e421928b8023678590e3
cybrd <brysuralta@gmail.com>
2024-07-26 18:36:16 +0800

    aws deploy 4

--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,10 @@
-export const handler = async () => {
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.handler = void 0;
+const handler = async () => {
     return {
         statusCode: 200,
         body: "Deployed",
     };
 };
+exports.handler = handler;

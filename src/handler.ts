ce1ec82fc752afcbd41087895cb9812fac0510ba
cybrd <brysuralta@gmail.com>
2024-07-26 21:47:04 +0800

    use express

--- src/handler.ts
+++ src/handler.ts
@@ -1,8 +1,24 @@
-import { Handler } from "aws-lambda";
+import serverless from "serverless-http";
+import express from "express";
 
-export const index: Handler = async () => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify("Deployed"),
-  };
-};
+const app = express();
+
+app.get("/", (req, res, next) => {
+  return res.status(200).json({
+    message: "Hello from root!",
+  });
+});
+
+app.get("/hello", (req, res, next) => {
+  return res.status(200).json({
+    message: "Hello from path!",
+  });
+});
+
+app.use((req, res, next) => {
+  return res.status(404).json({
+    error: "Not Found",
+  });
+});
+
+export const index = serverless(app);

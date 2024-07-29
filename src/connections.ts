5c0b3a057b88d552c11d2eeadfb2057e9b2fc2d1
cybrd <brysuralta@gmail.com>
2024-07-30 05:04:03 +0800

    debug mongodb uri

--- src/connections.ts
+++ src/connections.ts
@@ -2,6 +2,8 @@ import { MongoClient } from "mongodb";
 import { config } from "dotenv";
 config();
 
+console.log("process.env.MONGODB_URI", process.env.MONGODB_URI);
+
 export const mongoClient = new MongoClient(process.env.MONGODB_URI || "", {
   auth: {
     username: process.env.MONGODB_USERNAME || "",

3fefbbbbb4275e999a4120cb69e015a7701894a5
cybrd <brysuralta@gmail.com>
2024-07-30 04:45:42 +0800

    mongodb username password

--- src/connections.ts
+++ src/connections.ts
@@ -2,7 +2,12 @@ import { MongoClient } from "mongodb";
 import { config } from "dotenv";
 config();
 
-export const mongoClient = new MongoClient(process.env.MONGODB_URI || "");
+export const mongoClient = new MongoClient(process.env.MONGODB_URI || "", {
+  auth: {
+    username: process.env.MONGODB_USERNAME || "",
+    password: process.env.MONGODB_PASSWORD || "",
+  },
+});
 
 export const connectMongodb = () => {
   console.log("connectMongodb");

227d1cd59e066715c9044887275ae9258ceb2879
cybrd <brysuralta@gmail.com>
2024-07-30 04:34:17 +0800

    add cors and json

--- src/handler.ts
+++ src/handler.ts
@@ -1,9 +1,15 @@
 import serverless from "serverless-http";
-import express from "express";
+import express, { json, urlencoded } from "express";
+import cors from "cors";
+
 import { connectMongodb } from "./connections";
 import { userController } from "./controllers/user";
 
 const app = express();
+
+app.use(cors());
+app.use(urlencoded({ extended: false }));
+app.use(json());
 
 app.use(userController);
 

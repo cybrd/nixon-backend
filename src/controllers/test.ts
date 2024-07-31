6058d1d2a98f843c782b73ff53b8cfde32a1ee46
cybrd <brysuralta@gmail.com>
2024-07-31 18:21:16 +0800

    add test route with auth 2

--- src/controllers/test.ts
+++ src/controllers/test.ts
@@ -4,9 +4,9 @@ import { authUser } from "../middlewares/auth";
 export const testController = Router();
 
 testController.get("/supervisor", authUser("supervisor"), (req, res) => {
-  res.send(Math.random());
+  res.send(Math.random().toString());
 });
 
 testController.get("/admin", authUser("admin"), (req, res) => {
-  res.send(Math.random());
+  res.send(Math.random().toString());
 });

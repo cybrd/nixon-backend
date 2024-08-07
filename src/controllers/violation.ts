aefd341cff204b3975234f6f82a10bf24d989942
cybrd <brysuralta@gmail.com>
2024-08-07 20:54:37 +0800

    violation add filter

--- src/controllers/violation.ts
+++ src/controllers/violation.ts
@@ -6,20 +6,23 @@ import { mongoClient } from "../connections";
 
 import { getViolation, getViolationCount } from "../services/violation";
 import { authUser } from "../middlewares/auth";
+import { objectRemoveEmpty } from "../helper/object-remove-empty";
 
 export const violationController = Router();
 
 violationController.get("/", authUser("supervisor"), (req, res) => {
   (async () => {
-    const { page } = req.query;
+    const { page, ...filters } = req.query as { [k: string]: string };
+    const cleanFilter = objectRemoveEmpty(filters);
+
     let pageOption = 0;
     if (page) {
       pageOption = Number(page) - ONE;
     }
 
     const [data, counts] = await Promise.all([
-      getViolation(mongoClient, { limit: 25, page: pageOption }),
-      getViolationCount(mongoClient),
+      getViolation(mongoClient, { limit: 25, page: pageOption }, cleanFilter),
+      getViolationCount(mongoClient, cleanFilter),
     ]);
 
     res.send({
--- src/services/violation.ts
+++ src/services/violation.ts
@@ -4,25 +4,26 @@ import { Violation } from "../models/violation";
 
 export const getViolation = (
   client: MongoClient,
-  options = { limit: 25, page: 0 }
+  options = { limit: 25, page: 0 },
+  filter = {}
 ) => {
   console.log("getViolation");
 
   const collection = client.db("nixon").collection<Violation>("violation");
 
   return collection
-    .find({})
+    .find(filter)
     .skip(options.page * options.limit)
     .limit(options.limit)
     .toArray();
 };
 
-export const getViolationCount = (client: MongoClient) => {
+export const getViolationCount = (client: MongoClient, filter = {}) => {
   console.log("getViolationCount");
 
   const collection = client.db("nixon").collection<Violation>("violation");
 
-  return collection.countDocuments({});
+  return collection.countDocuments(filter);
 };
 
 export const createViolation = (client: MongoClient, data: Violation) => {

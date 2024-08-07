8e40cff4b580f02b2335b9bdb56c2b375d4f9173
cybrd <brysuralta@gmail.com>
2024-08-07 21:03:58 +0800

    violation customTransform

--- src/controllers/violation.ts
+++ src/controllers/violation.ts
@@ -10,10 +10,28 @@ import { objectRemoveEmpty } from "../helper/object-remove-empty";
 
 export const violationController = Router();
 
+const customTransform = (oldObj: Record<string, string>) => {
+  const newObj: typeof oldObj = {};
+
+  for (const prop in oldObj) {
+    if (typeof oldObj[prop] !== "undefined") {
+      switch (prop) {
+        case "fingerPrintId":
+          newObj.employeeNumber = oldObj[prop];
+          break;
+        default:
+          newObj[prop] = oldObj[prop];
+          break;
+      }
+    }
+  }
+  return newObj;
+};
+
 violationController.get("/", authUser("supervisor"), (req, res) => {
   (async () => {
-    const { page, ...filters } = req.query as { [k: string]: string };
-    const cleanFilter = objectRemoveEmpty(filters);
+    const { page, ...filters } = req.query as Record<string, string>;
+    const cleanFilter = customTransform(objectRemoveEmpty(filters));
 
     let pageOption = 0;
     if (page) {

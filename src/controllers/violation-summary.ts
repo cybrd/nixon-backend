067f112845bbbd6b3f860035470a61fad0d573fa
cybrd <brysuralta@gmail.com>
2024-08-23 06:47:21 +0800

    add date filter 2

--- src/controllers/violation-summary.ts
+++ src/controllers/violation-summary.ts
@@ -1,23 +1,58 @@
+import { Filter } from "mongodb";
 import { Router } from "express";
 import { StatusCodes } from "http-status-codes";
+import moment from "moment";
 
-import { ONE } from "../constants";
+import { ONE, ONE_THOUSAND } from "../constants";
 import { mongoClient } from "../connections";
 
 import { getViolation, getViolationCount } from "../services/violation";
+import { Violation } from "../models/violation";
 import { authUser } from "../middlewares/auth";
 import { getEmployees } from "../services/employee";
 import { objectRemoveEmpty } from "../helper/object-remove-empty";
 
 export const violationSummaryController = Router();
 
+const customFilterTransform = (filter: Record<string, string>) => {
+  const newFilter: Filter<Violation> = filter;
+
+  if (newFilter.dateFrom) {
+    if (!newFilter.parsedDateOfIncident) {
+      newFilter.parsedDateOfIncident = {};
+    }
+
+    newFilter.parsedDateOfIncident.$gte = new Date(
+      moment(newFilter.dateFrom).unix() * ONE_THOUSAND
+    );
+
+    delete newFilter.dateFrom;
+  }
+
+  if (newFilter.dateTo) {
+    if (!newFilter.parsedDateOfIncident) {
+      newFilter.parsedDateOfIncident = {};
+    }
+
+    newFilter.parsedDateOfIncident.$lte = new Date(
+      moment(newFilter.dateTo).unix() * ONE_THOUSAND
+    );
+
+    delete newFilter.dateTo;
+  }
+
+  return newFilter;
+};
+
 violationSummaryController.get("/:id", authUser("supervisor"), (req, res) => {
   (async () => {
     const { page, ...filters } = req.query as Record<string, string>;
-    const cleanFilter = objectRemoveEmpty({
-      ...filters,
-      employeeNumber: req.params.id,
-    });
+    const cleanFilter = customFilterTransform(
+      objectRemoveEmpty({
+        ...filters,
+        employeeNumber: req.params.id,
+      })
+    );
 
     let pageOption = 0;
     if (page) {
